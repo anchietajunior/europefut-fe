2e0a63c8a63441564af65e746d2e50ac6dccf7f5
anchietajunior <santosjr87@gmail.com>
2016-04-07 23:48:40 -0300

    Added login controller, view and called both in app.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the europefrontendApp
  */
 angular.module('europefrontendApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
